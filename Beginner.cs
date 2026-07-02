20e94c3f967e7c5349b230b6a709f6345a0015f3
Aaron <samlfstr@gmail.com>
2026-07-02 10:30:42 +0300

    Initial Commit

---
+++ Beginner.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Beginner
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Beginner");
+    }
+}                       
+
+
+
---
+++ Lesson_1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Lesson_1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Lesson_1");
+    }
+}                       
+
+
+
